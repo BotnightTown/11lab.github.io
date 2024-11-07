9c04c29bc04c736cb6be5003f22243d870f0a55c
BotnightTown <148649158+BotnightTown@users.noreply.github.com>
2024-11-07 20:41:01 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -34,9 +34,39 @@ const cardGame = () => {
   showCard();
   function showCard() {
     if (cards.length === 0) {
+      switch (true) {
+        case(wrongCount === 0):
+          $('#skill-level').text('прекрасний')
+          $('#skill-word').text("ідеально знаєте англійську")
+          break;
+        case(rightCount === 0):
+          $('#skill-level').text('дуже поганий')
+          $('#skill-word').text("не знаєте англійську взагалі")
+          break;
+        case (rightCount > wrongCount):
+          $('#skill-level').text("гарний")
+          $('#skill-word').text("вумний")
+          break;
+        case (rightCount < wrongCount):
+          $('#skill-level').text('поганий')
+          $('#skill-word').text("тупенький")
+          break;
+        case(rightCount === wrongCount):
+          $('#skill-level').text('задовільний')
+          $('#skill-word').text("студент чну")
+          break;
+        default:
+          break;
+      }
+
       $('#card').text("Всі слова закінчилися!");
       $('#next').prop('disabled', true);
       $('#word').prop('disabled', true);
+      $("#modal").css("display", 'block');
+      setTimeout(() => {
+        $('#modal').addClass('visible');
+        $('#modal-content').addClass('visible');
+      }, 1);
     }
     currentWord = cards[Math.floor(Math.random() * cards.length)];
     $('#card').text(currentWord);
